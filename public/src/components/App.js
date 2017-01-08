3467182e2f9a974c619285c1d2a3828787a18186
wangqi <774948439@qq.com>
2017-01-08 20:24:17 +0000

    modify people export component name

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'; // eslint-disable-line no-unused-vars
 import AddTodo from '../container/AddTodo';
 import ResultList from './ResultList';
-import AddPeople from '../container/AddPeople';
+import PeopleList from '../container/AddPeople';
 
 class App extends Component {
 
   render() {
     return (
         <div>
-          <AddPeople />
+          <PeopleList />
           <AddTodo />
           <ResultList />
         </div>
